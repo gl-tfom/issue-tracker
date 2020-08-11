5f1045e5c1ea3598a54b141dcf87778b514992ba
gl-tfom <14111335+gl-tfom@users.noreply.github.com>
2020-08-11 14:41:57 -0700

    test2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
-var app = express()
+var app = express();
 // use helmet to add more security options
 app.use(helmet());
 app.use(helmet.noSniff());
